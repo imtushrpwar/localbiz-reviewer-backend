31440579c9e3367f8480076b73c6dd6aac73aea6
Tushar Pawar <tp429141@gmail.com>
2026-08-08 17:58:14 +0530

    Prepare backend for deployment

--- src/server.js
+++ src/server.js
@@ -4,12 +4,13 @@ const app = require("./app");
 const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
+const HOST = "0.0.0.0";
 
 const startServer = async () => {
     await connectDB();
 
-    app.listen(PORT, () => {
-        console.log(`🚀 Server running on http://localhost:${PORT}`);
+    app.listen(PORT, HOST, () => {
+        console.log(`🚀 Server running on http://${HOST}:${PORT}`);
     });
 };
 
